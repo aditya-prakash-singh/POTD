8b97877233d40f5334baab1dcf066d65d5ae7afd
aditya-prakash-singh <126975871+aditya-prakash-singh@users.noreply.github.com>
2024-02-25 19:04:15 +0530

    Create main.py

---
+++ Day51/main.py
@@ -0,0 +1,10 @@
+lass Solution:
+    def count(self, n: int) -> int:
+        #your code here
+        dp = [0]*(n+1)
+        dp[0] = 1
+        
+        for i in [3,5,10]:
+          for j in range(i,n+1):
+            dp[j]+=dp[j-i]
+        return dp[n]
