cfe86e9567b854a86081b92b28809a426851d414
aditya-prakash-singh <126975871+aditya-prakash-singh@users.noreply.github.com>
2024-06-10 23:42:16 +0530

    '131'

---
+++ Day131/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        l=heights.copy()
+        heights.sort()
+        ans=0
+        for i in range(len(l)):
+            if l[i]!=heights[i]:
+                ans+=1
+        return ans
